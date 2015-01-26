ea138f91ed7591142e84e21603a681130e45705b
David Lindblad <david.lindblad@outlook.com>
2015-01-26 20:34:39 +0100

    Enhanced DSC publisher

--- IAC/src/Publish-DesiredStateConfiguration.ps1
+++ IAC/src/Publish-DesiredStateConfiguration.ps1
@@ -1,15 +1,19 @@
 . .\Helpers.ps1
 
+EnsureAuthentication
+
 $subscriptionName = Get-AzureSubscription | ? { $_.IsCurrent } | select -ExpandProperty SubscriptionName
 
 $storageAccountName = 'ifwpdsc'
 
 Set-AzureSubscription `
     -SubscriptionName $subscriptionName `
-    -CurrentStorageAccountName $storageAccountName
+    -CurrentStorageAccountName $storageAccountName `
+    -Verbose
 
 gci ..\dsc\ | % {
     Publish-AzureVMDscConfiguration `
         -ConfigurationPath $_.FullName `
-        -Force
+        -Force `
+        -Verbose
 }
